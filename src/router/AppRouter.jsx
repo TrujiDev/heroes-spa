644d1ca8a319eb57caa91669af3a21db497b1471
TrujiDev <eetgrisales@gmail.com>
2024-06-18 21:14:39 -0500

    Add route protection with PrivateRoute and PublicRoute
    - Import PrivateRoute and PublicRoute components
    - Wrap Login route with PublicRoute for public access control
    - Wrap HeroesRoutes with PrivateRoute for authenticated access control

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Login } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 	return (
 		<>
 			<Routes>
 				<Route
-					path='login'
-					element={<Login />}
+					path='/login'
+					element={
+						<PublicRoute>
+							<Login />
+						</PublicRoute>
+					}
 				/>
 
 				<Route
 					path='/*'
-					element={<HeroesRoutes />}
+					element={
+						<PrivateRoute>
+							<HeroesRoutes />
+						</PrivateRoute>
+					}
 				/>
 			</Routes>
 		</>
